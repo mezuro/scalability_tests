adb28f720be4e3bf1d1ac29699e31b98e440af49
Diego Araújo <diegoamc90@gmail.com>
2016-01-27 10:59:13 -0200

    Refactor LastReadyProcessing experiment, move it to the Repository pkg
    Signed-off-by: Daniel Miranda <danielkza2@gmail.com>

---
+++ src/REST/repositoryEndpoint/LastReadyProcessing.java
@@ -0,0 +1,53 @@
+package REST.repositoryEndpoint;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+
+import strategy.RESTStrategy;
+
+public class LastReadyProcessing extends RESTStrategy {
+
+	private String repositoryId;
+
+	@Override
+	public void beforeExperiment() throws Exception {
+		HashMap<String, HashMap<String, String>> parameters = new HashMap<String, HashMap<String, String>>();
+		HashMap<String, String> repository = new HashMap<String, String>();
+		parameters.put("repository", repository);
+		repository.put("address", "svn://svn.code.sf.net/p/qt-calculator/code/trunk");
+		repository.put("name", "name");
+		repository.put("scm_type", "SVN");
+		repository.put("kalibro_configuration_id", "1");
+		JSONObject jsonBody = new JSONObject(parameters);
+		HttpResponse<JsonNode> response = post(buildUrl(REPOSITORY_PATH), jsonBody);
+		repositoryId = ((JSONObject) (response.getBody().getObject().get("repository"))).get("id").toString();
+		
+		Thread.sleep(1000);
+		while (get(buildUrl(REPOSITORY_PATH + "/" + repositoryId + "/has_ready_processing"))
+				.getBody().getObject().get("has_ready_processing").equals("false")) {
+			Thread.sleep(1000);
+		}
+	}
+
+	@Override
+	public String request(String string) throws Exception {
+		get(buildUrl(REPOSITORY_PATH + "/" + repositoryId + "/last_ready_processing"));
+		return repositoryId;
+	}
+
+	@Override
+	public void afterExperiment() throws Exception {
+		delete(buildUrl(REPOSITORY_PATH + "/" + repositoryId));
+	}
+
+	@Override
+	public void configure(Map<Object, Object> options) {
+		configure(options, "kalibro_processor");
+	}
+
+}
