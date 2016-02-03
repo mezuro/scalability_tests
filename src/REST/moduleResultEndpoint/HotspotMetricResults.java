124e927667ab1655c9f37c8b75b7b232b27a5d36
Diego Araújo <diegoamc90@gmail.com>
2016-02-03 10:03:29 -0200

    Add ModuleResult#hotspot_metric_results experiment

---
+++ src/REST/moduleResultEndpoint/HotspotMetricResults.java
@@ -0,0 +1,58 @@
+package REST.moduleResultEndpoint;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONObject;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+
+import strategy.RESTStrategy;
+
+public class HotspotMetricResults extends RESTStrategy {
+	private String repositoryId;
+	private int root_module_result_id;
+
+	@Override
+	public void beforeExperiment() throws Exception {
+		HashMap<String, HashMap<String, String>> parameters = new HashMap<String, HashMap<String, String>>();
+		HashMap<String, String> repository = new HashMap<String, String>();
+		parameters.put("repository", repository);
+		repository.put("address", "svn://svn.code.sf.net/p/qt-calculator/code/trunk");
+		repository.put("name", "name");
+		repository.put("scm_type", "SVN");
+		repository.put("kalibro_configuration_id", "1");
+		JSONObject jsonBody = new JSONObject(parameters);
+		HttpResponse<JsonNode> response = post(buildUrl(REPOSITORY_PATH), jsonBody);
+		repositoryId = ((JSONObject) (response.getBody().getObject().get("repository"))).get("id").toString();
+
+		get(buildUrl(REPOSITORY_PATH + "/" + repositoryId + "/process"));
+
+		boolean hasReadyProcessing;
+		do {
+			Thread.sleep(1000);
+			hasReadyProcessing = get(buildUrl(REPOSITORY_PATH + "/" + repositoryId + "/has_ready_processing"))
+					.getBody().getObject().getBoolean("has_ready_processing");
+		} while(!hasReadyProcessing);
+		
+		HttpResponse<JsonNode> last_processing = get(buildUrl(REPOSITORY_PATH + "/" + repositoryId + "/last_ready_processing"));
+		root_module_result_id = last_processing.getBody().getObject().getJSONObject("last_ready_processing").getInt("root_module_result_id");
+	}
+
+	@Override
+	public String request(String string) throws Exception {
+		get(buildUrl(MODULE_RESULT_PATH + "/" + root_module_result_id + "/hotspot_metric_results"));
+		return repositoryId;
+	}
+
+	@Override
+	public void afterExperiment() throws Exception {
+		delete(buildUrl(REPOSITORY_PATH + "/" + repositoryId));
+	}
+
+	@Override
+	public void configure(Map<Object, Object> options) {
+		configure(options, "kalibro_processor");
+	}
+}
