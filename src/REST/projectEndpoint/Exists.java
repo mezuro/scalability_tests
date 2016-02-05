09a4a4f3b9f0bb6e9f4ef49d76e0b995bdfab320
Diego Araújo <diegoamc90@gmail.com>
2016-02-05 14:21:33 -0200

    Fix Project#exists experiment

--- src/REST/projectEndpoint/Exists.java
+++ src/REST/projectEndpoint/Exists.java
@@ -1,39 +1,15 @@
 package REST.projectEndpoint;
 
-import java.util.HashMap;
 import java.util.Map;
-
-import org.json.JSONObject;
-
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
 
 import strategy.RESTStrategy;
 
 public class Exists extends RESTStrategy {
-
-	private String projectId;
-
-	@Override
-	public void beforeExperiment() throws Exception {
-		HashMap<String, HashMap<String, String>> parameters = new HashMap<String, HashMap<String, String>>();
-		HashMap<String, String> project = new HashMap<String, String>();
-		project.put("name", "Project");
-		parameters.put("project", project);
-		JSONObject jsonBody = new JSONObject(parameters);
-		HttpResponse<JsonNode> response = post(buildUrl(PROJECT_PATH), jsonBody);
-		projectId = ((JSONObject) (response.getBody().getObject().get("project"))).get("id").toString();
-	}
 	
 	@Override
 	public String request(String string) throws Exception {
-		get(buildUrl(PROJECT_PATH + "/" + projectId));
+		get(buildUrl(PROJECT_PATH + "/1/exists"));
 		return null;
-	}
-	
-	@Override
-	public void afterExperiment() throws Exception {
-		delete(buildUrl(PROJECT_PATH + "/" + projectId));
 	}
 
 	@Override
